12e49e7fdbf506ae8bfda1a48d883e8f0f65796f
James Collins <james@om4.com.au>
2014-07-30 10:11:54 +0800

    Initial release

---
+++ logout-shortcode.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Log Out Shortcode
+Plugin URI: http://om4.com.au/wordpress-plugins/
+Description: Add a log out link using a simple [logout] shortcode.
+Version: 1.0
+Author: OM4
+Author URI: http://om4.com.au/
+Text Domain: logout-shortcode
+Git URI: https://github.com/OM4/logout-shortcode
+Git Branch: release
+License: GPLv2
+*/
+
+/*
+
+   Copyright 2014 OM4 (email: info@om4.com.au    web: http://om4.com.au/)
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function logout_shortcode( $atts, $content = null, $shortcode_name = null ) {
+	$defaults = array(
+		'text' => __('Log out'), // This is the default log out text from wp-includes/general-template.php
+		'redirect' => '', // URL to redirect to after logging out
+		'class' => 'logout' //
+	);
+
+	if ( !is_user_logged_in() ) {
+		return '';
+	}
+
+	$atts = shortcode_atts( $defaults, $atts );
+
+	if ( 'logout_to_home' == $shortcode_name ) {
+		$atts['redirect'] = 'home';
+	} else if ( 'logout_to_current' == $shortcode_name ) {
+		$atts['redirect'] = 'current';
+	}
+
+	if ( 'home' == $atts['redirect'] ) {
+			$atts['redirect'] = home_url();
+	} else if ( 'current' == $atts['redirect'] ) {
+		$atts['redirect'] = get_the_permalink();
+	}
+
+	return '<a href="' . esc_url( wp_logout_url( $atts['redirect'] ) ) . '" class="' . sanitize_html_class( $atts['class'] ) . '" >'  . esc_html( $atts['text'] ) . '</a>';
+
+}
+add_shortcode( 'logout', 'logout_shortcode' );
+add_shortcode( 'logout_to_current', 'logout_shortcode' );
+add_shortcode( 'logout_to_home', 'logout_shortcode' );
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== Log Out Shortcode ===
+Tags: logout, shortcode, log-out, log out, link
+Contributors: jamesacollins
+Requires at least: 3.7
+Tested up to: 3.9.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add a log out link using a simple shortcode.
+
+== Description ==
+
+There are 3 shortcodes that can be used:
+
+`[logout]` - A standard Log Out link, where the user stays on the login screen after being logged out.
+`[logout_to_home]` - A link that logs the user out, and redirects them to your home page.
+`[logout_to_current]` - A link that logs the user out, and redirects them back to the current page/URL.
+
+**Advanced Usage:**
+
+All 3 shortcodes all support 3 attributes/parameters:
+
+`[logout text="" class="" redirect=""]`
+
+The 3 supported attributes/parameters are:
+
+`text=""`
+- The text to use for the link anchor text.
+- Default: `Log Out`
+`class=""`
+- The HTML class(es) to use for the link. Multiple classes should be separated by a space.
+- Default: `logout`
+`redirect=""`
+- The URL to redirect the user to after they are logged out.
+- Default: the WordPress login screen.
+
+**Some Examples:**
+`[logout text="Log Off"]` - A `Log Off` link that logs the user out.
+`[logout redirect="/members/"]` - Log the user out, and redirect them to `/members/`.
+`[logout_to_home text="Log Off"]` - A `Log Off` link that logs the user out and redirects them to your home page.
+`[logout class="log-out-link"]` - Use a custom CSS/HTML class name for the log out link.
+
+
+Note: the shortcodes will return nothing/empty if the user isn't logged in.
+
+
+This plugin is inspired by the [Logout Button plugin by Ben Poland](http://wordpress.org/plugins/logout-button/).
+
+== Installation ==
+
+1. Activate the plugin.
+1. Add one of the shortcodes to a post/page.
+
+== Frequently Asked Questions ==
+
+= What shortcodes can be used? =
+
+Please see the plugin description for a list of shortcodes and supported parameters.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
