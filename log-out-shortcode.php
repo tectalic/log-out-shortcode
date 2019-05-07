eb622dccac28518793cc122d88210d31b8b4c7f1
Csaba Maulis <csaba@om4.com.au>
2019-05-07 16:42:30 +0800

    Cleanup code with phpcs

--- log-out-shortcode.php
+++ log-out-shortcode.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 Plugin Name: Log Out Shortcode
 Plugin URI: https://om4.com.au/plugins/log-out-shortcode/
 Description: Easily add a log out link to a post or page using a simple <code>[logout]</code> shortcode.
@@ -8,32 +8,39 @@ Author: OM4
 Author URI: https://om4.com.au/plugins/
 Text Domain: log-out-shortcode
 License: GPLv2
-*/
+ */
 
 /*
+Copyright 2014-2016 OM4 (email: plugins@om4.com.au    web: https://om4.com.au/)
 
-   Copyright 2014-2016 OM4 (email: plugins@om4.com.au    web: https://om4.com.au/)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-function logout_shortcode( $atts, $content = null, $shortcode_name = null ) {
+/**
+ * Create logout link
+ *
+ * @param array  $atts           The shortcode attributes.
+ * @param string $content        The shortcode content (if any).
+ * @param string $shortcode_name Name of the shortcode.
+ * @return string
+ */
+function logout_shortcode( $atts, $content = '', $shortcode_name = '' ) {
 	$defaults = array(
-			'text'     => __( 'Log out' ), // This is the default log out text from wp-includes/general-template.php
-			'redirect' => '', // path/URL to redirect to after logging out
-			'class'    => 'logout', // CSS Class(es) to use in link.
+		'text'     => __( 'Log out' ), // This is the default log out text from wp-includes/general-template.php.
+		'redirect' => '', // path/URL to redirect to after logging out.
+		'class'    => 'logout', // CSS Class(es) to use in link.
 	);
 
 	if ( ! is_user_logged_in() ) {
@@ -42,27 +49,27 @@ function logout_shortcode( $atts, $content = null, $shortcode_name = null ) {
 
 	$atts = shortcode_atts( $defaults, $atts );
 
-	if ( 'logout_to_home' == $shortcode_name ) {
+	if ( 'logout_to_home' === $shortcode_name ) {
 		$atts['redirect'] = 'home';
-	} else if ( 'logout_to_current' == $shortcode_name ) {
+	} elseif ( 'logout_to_current' === $shortcode_name ) {
 		$atts['redirect'] = 'current';
 	}
 
-	if ( 'home' == $atts['redirect'] ) {
+	if ( 'home' === $atts['redirect'] ) {
 			$atts['redirect'] = home_url();
-	} else if ( 'current' == $atts['redirect'] ) {
+	} elseif ( 'current' === $atts['redirect'] ) {
 		$atts['redirect'] = get_the_permalink();
 	}
 
 	$class_html = '';
-	if ( '' != $atts['class'] ) {
-		// Multiple classes are separated by a space
-		$classes = explode( ' ', $atts['class'] );
-		$classes = array_map( 'sanitize_html_class', $classes );
+	if ( '' !== $atts['class'] ) {
+		// Multiple classes are separated by a space.
+		$classes    = explode( ' ', $atts['class'] );
+		$classes    = array_map( 'sanitize_html_class', $classes );
 		$class_html = ' class="' . implode( $classes, ' ' ) . '"';
 	}
 
-	return '<a href="' . esc_url( wp_logout_url( $atts['redirect'] ) ) . '"' . $class_html . '>'  . esc_html( $atts['text'] ) . '</a>';
+	return '<a href="' . esc_url( wp_logout_url( $atts['redirect'] ) ) . '"' . $class_html . '>' . esc_html( $atts['text'] ) . '</a>';
 
 }
 add_shortcode( 'logout', 'logout_shortcode' );
